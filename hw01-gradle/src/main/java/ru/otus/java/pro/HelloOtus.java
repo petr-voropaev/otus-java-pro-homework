5066ad9e667b405d9e2425e288fc3577054e8591
Petr Voropaev <darksilver.g@gmail.com>
2025-01-20 00:53:02 +0700

    fixed build.gradle

--- hw01-gradle/src/main/java/ru/otus/java/pro/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/java/pro/HelloOtus.java
@@ -2,6 +2,7 @@ package ru.otus.java.pro;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 
+@SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String[] args) {
         String str = "Hello Otus!";
