c94fae055a915ca37b98651c1f02329dd6c9432a
petr voropaev <darksilver.y@yandex.ru>
2026-04-01 20:34:09 +0700

    [HW-21] tests implementation in hw21-jpql

--- hw21-jpql/homework-template/src/main/java/ru/otus/crm/model/Address.java
+++ hw21-jpql/homework-template/src/main/java/ru/otus/crm/model/Address.java
@@ -1,7 +1,47 @@
 package ru.otus.crm.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
 public class Address {
+
+    @Id
+    @SequenceGenerator(name = "address_gen", sequenceName = "address_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street", length = 250, nullable = false)
+    private String street;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
     public Address(Long id, String street) {
-        throw new UnsupportedOperationException();
+        this.id = id;
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" + "id=" + id + ", street='" + street + '\'' + ", client=" + client + '}';
     }
 }
