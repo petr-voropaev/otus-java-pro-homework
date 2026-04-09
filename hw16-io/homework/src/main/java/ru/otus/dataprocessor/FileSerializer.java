40c7a8eb9d5fd2c2107d2bc9febb04eeeb2b66a0
petr voropaev <darksilver.y@yandex.ru>
2026-04-09 16:57:43 +0700

    [HW-16] todo implementation in hw16-io

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,25 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+        var mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(fileName), data);
+        } catch (IOException ex) {
+            throw new FileProcessException(ex);
+        }
     }
 }
